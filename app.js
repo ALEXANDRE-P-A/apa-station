debd77bb036b7b43fde5c17de910e78f817be548
Alexandre <alexandrepereiraassano@gmail.com>
2025-10-14 23:18:33 +0900

    add incomplete todo-app

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/src/index.html"));
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+app.get("/todo-app", (req, res) => {
+  app.use("/static", express.static(path.join(__dirname, "build", "static")));
+  app.use("/", express.static(path.join(__dirname, "build")));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT,_=>{
