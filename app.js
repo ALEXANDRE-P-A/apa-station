a23e054b7869cb9dad2eb67e8cc807faf5bacd02
Alexandre <alexandrepereiraassano@gmail.com>
2025-10-14 15:13:16 +0900

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ "message": "apa-station" });
+  res.sendFile(path.join(__dirname, "/src/index.html"));
 });
 
 app.listen(PORT,_=>{
